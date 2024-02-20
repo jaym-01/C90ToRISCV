ffd68c88744bf133d2f6fbff0982b70a2dad16bc
John Yeo <51376134+johnyeocx@users.noreply.github.com>
2024-02-20 22:25:03 +0000

    added single test run script

---
+++ run.sh
@@ -0,0 +1,10 @@
+# "compiler_tests/_example/example.c"
+
+make bin/c_compiler
+rm -f bin/riscv_example.s
+./bin/c_compiler -S "compiler_tests/_example/example.c" -o "bin/riscv_example.s"
+
+riscv64-unknown-elf-gcc -march=rv32imfd -mabi=ilp32d -o "bin/riscv_example" "bin/riscv_example.s" "compiler_tests/_example/example_driver.c"
+
+set +e
+spike pk "bin/riscv_example"
---
+++ trial.s
@@ -0,0 +1,5 @@
+.text
+.globl f
+f:
+li a0, 5
+ret
